f2f971767bdb4f1b21bb380eb179ce6666b032bf
TA <appulegugure@gmail.com>
2019-06-26 21:51:45 +0900

    lei

---
+++ AppFirst/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  AppFirst
+//
+//  Created by アップルググれ on 2019/06/26.
+//  Copyright © 2019 アップルググ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    class Node{
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
+
