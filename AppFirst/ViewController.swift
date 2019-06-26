9ce5c72394f298bbe7c09511ff43a01066a8c898
TA <appulegugure@gmail.com>
2019-06-27 07:19:41 +0900

    削除

--- AppFirst/ViewController.swift
+++ AppFirst/ViewController.swift
@@ -9,12 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    class Node{
-        let saa = "nio"
-        let hu = 8
-        let huj = 3
-        let we = "888"
-    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
